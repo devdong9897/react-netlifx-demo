006d78ef707464fbceb6e38096a20d7a0d6cfe42
devdong9897 <dev.dong9897@gmail.com>
2025-01-15 17:30:10 +0900

    feat: Route 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./layout/AppLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 // 홈페이지 /
-// 영화 전체 보여주는 페이지 (서치) /movie
-// 영화 디테일 페이지 /
+// 영화 전체 보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route path="" />
+        <Route index element={<HomePage />} />
+        {/* movies를 사용하는 Route */}
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
       </Route>
+      
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
