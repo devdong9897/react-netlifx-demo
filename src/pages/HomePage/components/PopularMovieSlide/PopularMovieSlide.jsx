bb96c99fdde57e7415d8ab8da9f616e70733b1a7
devdong9897 <dev.dong9897@gmail.com>
2025-01-16 14:44:05 +0900

    feat: 영화 슬라이드 구현

--- src/pages/HomePage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/HomePage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import Alert from "react-bootstrap/Alert";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import MovieCard from "../MovieCard/MovieCard";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 8,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -12,9 +30,20 @@ const PopularMovieSlide = () => {
   if (isError) {
     return <Alert variant="danger">{error.message}</Alert>;
   }
+
   return (
     <div>
-      <h3>Popular Movies</h3>
+      <Carousel
+        infinite={true}
+        centerMode={true}
+        itemClass="movie-slider p-1"
+        containerClass="carousel-container"
+        responsive={responsive}
+      >
+        {data.results.map((movie, index) => (
+          <MovieCard movie={movie} key={index} />
+        ))}
+      </Carousel>
     </div>
   );
 };
